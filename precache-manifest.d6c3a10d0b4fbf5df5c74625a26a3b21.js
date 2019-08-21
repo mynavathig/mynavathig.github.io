08c9dcf546115d1f1a8a0f6b5b562f845c96aea0
unknown <mynaksrit@gmail.com>
2019-08-21 20:11:58 +0530

    Initial commit

---
+++ precache-manifest.d6c3a10d0b4fbf5df5c74625a26a3b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07949f678b8754f7735c4f9f294266b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51e02c608b5e05090342",
+    "url": "/static/css/main.e0787004.chunk.css"
+  },
+  {
+    "revision": "d40d7b6d72c873e17df1",
+    "url": "/static/js/2.2b000784.chunk.js"
+  },
+  {
+    "revision": "51e02c608b5e05090342",
+    "url": "/static/js/main.42bdd9e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9c482827bad96acade4bdb916c68641f",
+    "url": "/static/media/Mynavathi.9c482827.pdf"
+  },
+  {
+    "revision": "52c665a94ac97c10929f3396e5408d85",
+    "url": "/static/media/ProfileImage.52c665a9.jpg"
+  }
+]);
